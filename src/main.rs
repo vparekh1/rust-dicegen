7147f1530db97c7046603a7449c4dd7347e82a44
Vivek Parekh <vp@vivekparekh.ca>
2020-06-20 20:48:16 -0400

    Fix the tests with optionals so that they pass

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ mod parser;
 use rand::Rng;
 
 fn main() {
-    let rng = rand::thread_rng();
-    let mut request = roll::RollRequest::new(10000, 1000, Some(500), rng);
+    // let rng = rand::thread_rng();
+    // let mut request = roll::RollRequest::new(10000, 1000, Some(500), rng);
 
-    println!("{:?}", request.roll_dice());
+    // println!("{:?}", request.roll_dice());
 }
--- src/roll.rs
+++ src/roll.rs
@@ -11,43 +11,68 @@ struct AggregatedResult {
 
 pub struct RollRequest<R: Rng> {
     number_of_rolls: u64,
-    dice: u64,               // d
-    explode_on: Option<u64>, // e
-    result: Vec<u64>,
-    rng: R
-}
-
-struct RollModifier {
+    dice: u64,                     // d
+    explode_on: Option<u64>,       // e
     remove_count: Option<u64>,     // r
     top_limit: Option<u64>,        // t
     failure_limit: Option<u64>,    // f
     keep_count: Option<u64>,       // k
     keep_lower_count: Option<u64>, // kl
+    result: Vec<u64>,
+    rng: R,
 }
 
-// impl AggregatedResult {
-//     pub fn new(mut dice_rolls: Vec<u64>, modifiers: RollModifier) -> AggregatedResult {
-//         dice_rolls.sort();
-//         let kept_rolls = dice_rolls.iter().skip(modifiers.remove_count);
-
-//         AggregatedResult {
-//             kept_rolls: kept_rolls,
-//             sum: 0,
-//             count: 0,
-//         }
-//     }
-
-// }
-
 impl<R: Rng> RollRequest<R> {
-    pub fn new(number_of_rolls: u64, dice: u64, explode_on: Option<u64>, rng: R) -> RollRequest<R> {
+    pub fn new(
+        number_of_rolls: u64,
+        dice: u64,
+        explode_on: Option<u64>,
+        remove_count: Option<u64>,
+        top_limit: Option<u64>,
+        failure_limit: Option<u64>,
+        keep_count: Option<u64>,
+        keep_lower_count: Option<u64>,
+        rng: R,
+    ) -> RollRequest<R> {
         RollRequest {
             number_of_rolls,
             dice,
             explode_on,
+            remove_count,
+            top_limit,
+            failure_limit,
+            keep_count,
+            keep_lower_count,
             result: vec![],
-            rng
+            rng,
         }
+    }
+
+    fn new_simple(
+        number_of_rolls: u64,
+        dice: u64,
+        explode_on: Option<u64>,
+        rng: R,
+    ) -> RollRequest<R> {
+        RollRequest {
+            number_of_rolls,
+            dice,
+            explode_on,
+            remove_count: None,
+            top_limit: None,
+            failure_limit: None,
+            keep_count: None,
+            keep_lower_count: None,
+            result: vec![],
+            rng,
+        }
+    }
+
+    pub fn gen(&mut self) {
+        self.roll_dice();
+        self.remove(self.remove_count);
+        self.keep(self.keep_count);
+        self.keep_lower(self.keep_lower_count);
     }
 
     pub fn roll_dice(&mut self) {
@@ -64,15 +89,30 @@ impl<R: Rng> RollRequest<R> {
         self.result = result;
     }
 
-    pub fn remove_count(&mut self, count: Option<u64>) {
-        if count.is_some() {
-            self.result = self.result[count.unwrap() as usize..].to_vec();
+    fn remove(&mut self, count: Option<u64>) {
+        if let Some(c) = count {
+            if self.result.len() > c as usize {
+                self.result = self.result[c as usize..].to_vec();
+            } else {
+                self.result = vec![];
+            }
         }
     }
 
-    pub fn keep_count(&mut self, count: Option<u64>) {
-        if count.is_some() {
-            self.result = self.result[count.unwrap() as usize..].to_vec();
+    fn keep(&mut self, count: Option<u64>) {
+        if let Some(c) = count {
+            if self.result.len() > c as usize {
+                let keep_index = self.result.len() - c as usize;
+                self.result = self.result[keep_index..].to_vec();
+            }
+        }
+    }
+
+    fn keep_lower(&mut self, count: Option<u64>) {
+        if let Some(c) = count {
+            if self.result.len() > c as usize {
+                self.result = self.result[..c as usize].to_vec();
+            }
         }
     }
 
@@ -86,13 +126,14 @@ impl<R: Rng> RollRequest<R> {
 }
 
 #[cfg(test)]
+
 mod tests {
     use super::*;
 
     #[test]
     fn roll_request_returns_roll_result() {
         let rng = rand::rngs::mock::StepRng::new(2, 1);
-        let mut request = RollRequest::new(5, 10, Some(5), rng);
+        let mut request = RollRequest::new_simple(5, 10, Some(5), rng);
         request.roll_dice();
         assert_eq!(request.result, vec![1, 1, 1, 1, 1]);
 
@@ -104,7 +145,7 @@ mod tests {
     #[test]
     fn proper_number_of_elements_in_exploded_dice_roll() {
         let rng = rand::thread_rng();
-        let mut request = RollRequest::new(10000, 1000, Some(500), rng);
+        let mut request = RollRequest::new_simple(10000, 1000, Some(500), rng);
         request.roll_dice();
 
         let expected_number_of_rolls = request.number_of_rolls
@@ -121,18 +162,81 @@ mod tests {
     #[test]
     fn remove_count_removes_counts() {
         let rng = rand::rngs::mock::StepRng::new(2, 1);
-        let mut request = RollRequest::new(5, 10, Some(5), rng);
+        let mut request = RollRequest::new_simple(5, 10, Some(5), rng);
 
         // Works when its Some(x)
         request.roll_dice();
         assert_eq!(request.result, vec![1, 1, 1, 1, 1]);
-        request.remove_count(Some(2));
+        request.remove(Some(2));
         assert_eq!(request.result, vec![1, 1, 1]);
 
         // Works when it's None
         request.roll_dice();
+        request.remove(None);
         assert_eq!(request.result, vec![1, 1, 1, 1, 1]);
-        request.remove_count(None);
+
+        // Works when its OOB
+        request.roll_dice();
+        request.remove(Some(5));
+        assert_eq!(request.result, vec![]);
+
+        // Works when its OOB
+        request.roll_dice();
+        request.remove(Some(6));
+        assert_eq!(request.result, vec![]);
+    }
+
+    #[test]
+    fn keep_count_keeps_the_right_number_of_values() {
+        let rng = rand::rngs::mock::StepRng::new(2, 1);
+        let mut request = RollRequest::new_simple(5, 10, Some(5), rng);
+
+        // Works when its Some(x)
+        request.roll_dice();
+        assert_eq!(request.result, vec![1, 1, 1, 1, 1]);
+        request.keep(Some(2));
+        assert_eq!(request.result, vec![1, 1]);
+
+        // Works when it's None
+        request.roll_dice();
+        request.keep(None);
+        assert_eq!(request.result, vec![1, 1, 1, 1, 1]);
+
+        // Works when its OOB
+        request.roll_dice();
+        request.keep(Some(5));
+        assert_eq!(request.result, vec![1, 1, 1, 1, 1]);
+
+        // Works when its OOB
+        request.roll_dice();
+        request.keep(Some(6));
+        assert_eq!(request.result, vec![1, 1, 1, 1, 1]);
+    }
+
+    #[test]
+    fn keep_lower_count_keeps_the_right_number_of_values() {
+        let rng = rand::rngs::mock::StepRng::new(2, 1);
+        let mut request = RollRequest::new_simple(5, 10, Some(5), rng);
+
+        // Works when its Some(x)
+        request.roll_dice();
+        assert_eq!(request.result, vec![1, 1, 1, 1, 1]);
+        request.keep_lower(Some(2));
+        assert_eq!(request.result, vec![1, 1]);
+
+        // Works when it's None
+        request.roll_dice();
+        request.keep_lower(None);
+        assert_eq!(request.result, vec![1, 1, 1, 1, 1]);
+
+        // Works when its OOB
+        request.roll_dice();
+        request.keep_lower(Some(5));
+        assert_eq!(request.result, vec![1, 1, 1, 1, 1]);
+
+        // Works when its OOB
+        request.roll_dice();
+        request.keep_lower(Some(6));
         assert_eq!(request.result, vec![1, 1, 1, 1, 1]);
     }
 }
